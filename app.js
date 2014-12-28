b685b57fe53a7279d76935472b9667345ebed3b5
Joffrey F <f.joffrey@gmail.com>
2014-12-27 18:26:17 -0800

    Updated app.js to use index and render content files

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-var express = require('express')
-var app = express()
+var express = require('express');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var middlewares = require('./sakuya/middlewares'),
+    content = require('./sakuya/content-processing');
 
-var server = app.listen(1990, function () {
+var app = express();
 
-  var host = server.address().address
-  var port = server.address().port
+console.log('Processing content files...')
+content.generateIndex(function(err, index) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(
+        'Index generated, ' + Object.keys(index.articles).length + 
+        ' articles found.'
+    )
+    for (articleName in index.articles) {
+        var article = index.articles[articleName];
+        app.get('/' + articleName, 
+            middlewares.render_markdown(index), 
+            function(req, res) {
+                res.setHeader('Content-Type', 'text/html');
+                // FIXME: Previous, next, header, footer, tags...
+                res.end(req.articleHTML);
+            }
+        );
+    }
+    app.get('/', function(req, res) {
+        res.redirect('/' + index.first);
+    });
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    var server = app.listen(1990, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+        console.log('Sakuya listening at http://%s:%s', host, port);
+    });
 
-})
+});
