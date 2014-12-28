480d1e2eaa9a54911aa572d760f721dc21046d3d
Joffrey F <f.joffrey@gmail.com>
2014-12-27 18:23:40 -0800

    utils

---
+++ sakuya/utils.js
@@ -0,0 +1,19 @@
+var _ = require('underscore');
+
+// Callback will be called either
+// * if an error is encountered during one of the calls
+// * after `length` successful iterations
+// if successful second argument will be a list of all the results
+function forEachCallback(length, callback) {
+	delayed = _.after(length, callback);
+	delayed.res = [];
+	return function(err, res) {
+		if (err) {
+			return callback(err);
+		}
+		delayed.res.push(res);
+		delayed(err, delayed.res);
+	}
+}
+
+module.exports.forEachCallback = forEachCallback;
